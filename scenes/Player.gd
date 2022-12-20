79c7fc431fe64ee58f228edbbc6eeb0059f41d05
Amiiiiiiiin <90452342+Amiiiiiiiin@users.noreply.github.com>
2022-12-20 02:45:01 +0100

    Reupload with new assets among other changes

---
+++ scenes/Player.gd
@@ -0,0 +1,69 @@
+extends KinematicBody2D
+
+signal died
+
+var gravity = 1000
+var velocity = Vector2.ZERO
+var maxHorizontalSpeed = 140
+var horizontalAcceleration = 2000
+var jumpSpeed = 360
+var jumpTerminationMultiplier = 3
+var hasDoubleJump = false
+
+func _ready():
+	$HazardArea.connect("area_entered", self, "on_hazard_area_entered")
+
+func _process(delta):
+	var moveVector = get_movement_vector()
+	
+	velocity.x += moveVector.x * horizontalAcceleration * delta
+	
+	if (moveVector.x == 0):
+		velocity.x = lerp(0, velocity.x, pow(2, -30 * delta))
+		
+	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed)
+	
+	if (moveVector.y < 0 && (is_on_floor() || !$CoyoteTimer.is_stopped() || hasDoubleJump)):
+		velocity.y = moveVector.y * jumpSpeed
+		if (!is_on_floor() && $CoyoteTimer.is_stopped()):
+			hasDoubleJump = false
+		$CoyoteTimer.stop()
+		
+	if (velocity.y < 0 && !Input.is_action_pressed("jump")):
+		velocity.y += gravity * jumpTerminationMultiplier * delta
+	else:
+		velocity.y += gravity * delta
+	
+	var wasOnFloor = is_on_floor()
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	if (wasOnFloor && !is_on_floor()):
+		$CoyoteTimer.start()
+	
+	if (is_on_floor()):
+		hasDoubleJump = true
+	
+	update_animation()
+	
+func get_movement_vector():
+	var moveVector = Vector2.ZERO
+	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
+	return moveVector
+
+func update_animation():
+	var moveVec = get_movement_vector()
+	
+	if (!is_on_floor()):
+		$AnimatedSprite.play("jump")
+	elif (moveVec.x !=0):
+		$AnimatedSprite.play("run")
+	else:
+		$AnimatedSprite.play("idle")
+	
+	if (moveVec.x != 0):
+		$AnimatedSprite.flip_h = true if moveVec.x < 0 else false
+
+func on_hazard_area_entered(_area2d):
+	emit_signal("died")
+	print("die")
